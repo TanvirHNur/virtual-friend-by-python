1d4b74fd5542f8b5280c150bf2f6cd9d2f2e4bd4
TanvirHNur <tanvir.du.ier@gmail.com>
2021-08-16 06:31:18 +0600

    first commit

---
+++ index.py
@@ -0,0 +1,9 @@
+# search on google python text to speech and go in that website pyttsx3 and copy pip install pyttsx3 and paste your terminal and wait till installation completed(If don't have pip, install pip)
+
+import pyttsx3 #import the library
+freind = pyttsx3.init() # creating friend ad initialization
+# if you need much more friends, create as many friend as needs
+# speech =  input ('Say Something: ')
+# freind.say(speech) #this bot will tell you this 
+freind.say("You are dashing smart") #this bot will tell you this 
+freind.runAndWait()
